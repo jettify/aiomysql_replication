ca786e6b691fbdabf290ca5661d372591fa523b7
Nickolai Novik <is.infinity@yahoo.com>
2015-06-09 23:41:42 +0300

    add setup.py and Manifest.in

---
+++ setup.py
@@ -0,0 +1,66 @@
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = ['PyMySQL<=0.6.6', 'aiomysql']
+
+PY_VER = sys.version_info
+
+if PY_VER >= (3, 4):
+    pass
+elif PY_VER >= (3, 3):
+    install_requires.append('asyncio')
+else:
+    raise RuntimeError("aiomysql doesn't suppport Python earllier than 3.3")
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+extras_require = {'sa': ['sqlalchemy>=0.9'], }
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'aiomysql_replication', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in '
+                               'aiomysql_replication/__init__.py')
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Operating System :: POSIX',
+    'Environment :: Web Environment',
+    'Development Status :: 3 - Alpha',
+    'Topic :: Database',
+    'Topic :: Database :: Front-Ends',
+]
+
+
+setup(name='aiomysql_replication',
+      version=read_version(),
+      description=('asyncio implementation of MySQL replication protocol'),
+      long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+      classifiers=classifiers,
+      platforms=['POSIX'],
+      author="Nikolay Novik",
+      author_email="nickolainovik@gmail.com",
+      url='https://github.com/jettify/aiomysql_replication',
+      download_url='https://pypi.python.org/pypi/aiomysql_replication',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=install_requires,
+      extras_require=extras_require,
+      include_package_data = True)
